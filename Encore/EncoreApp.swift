41d239df82b7a46811c6e3aaeafc622218b84225
NDC Swift <noah_carpenter@hotmail.com>
2026-06-15 14:34:35 -0400

    Initial Commit

---
+++ Encore/EncoreApp.swift
@@ -0,0 +1,24 @@
+//
+        //
+    //  Project: Encore
+    //  File: EncoreApp.swift
+    //  Created by Noah Carpenter 
+    //
+    //  📺 YouTube: Noah Does Coding
+    //  https://www.youtube.com/@NoahDoesCoding
+    //  Like and Subscribe for coding tutorials and fun! 💻✨
+    //  Dream Big. Code Bigger 🚀
+    //
+
+    
+
+import SwiftUI
+
+@main
+struct EncoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
